ddf01698f652850a1cd7ebe84935bb036ec2ca48
wpiskaa <hafizzkurniawan19@gmail.com>
2025-06-02 09:56:30 +0700

    membuat void setXU string a dan b

--- abstraksi/abstraksi.cpp
+++ abstraksi/abstraksi.cpp
@@ -3,4 +3,13 @@ using namespace std;
 
 class AbstraksiKlas{
     private: string x, y;
+
+    public:
+
+    //method untuk mengisi nilao
+    //private member
+    void setXU(string a, string b){
+        x = a;
+        y = b;
+    }
 };
