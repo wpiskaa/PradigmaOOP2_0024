b3e4b6374c81f47310a5429d11f718675b543cff
wpiskaa <hafizzkurniawan19@gmail.com>
2025-06-02 10:44:48 +0700

    membuat derivedclass beserta fungsi fungsi didalamnya

--- noOverriding.cpp/noOverriding.cpp
+++ noOverriding.cpp/noOverriding.cpp
@@ -7,3 +7,9 @@ class baseClass{
         cout << "Hallo saya function dari base class";
     }
 };
+class derivedClass : public baseClass {
+    public:
+    void perkenalan(){
+        cout << "Hallo saya function dari derived Class";
+    }
+};
