a5d2fab2ee205ed8752f9dce6bdbf40831bc4c58
wpiskaa <hafizzkurniawan19@gmail.com>
2025-06-02 11:11:47 +0700

    membuat class joko dan lia beserta fungsi didalamnya

--- polimorpi/polimorpi.cpp
+++ polimorpi/polimorpi.cpp
@@ -8,3 +8,17 @@ class seseorang {
     //  cout << "Pesan dari seseorang" << endl;
     // }
 };
+
+class joko :public seseorang{
+    public:
+    void pesan(){
+        cout << "Pesan dari joko" << endl;
+    }
+};
+
+class lia :public seseorang{
+    public:
+    void pesan(){
+        cout << "Pesan dari lia" << endl;
+    }
+};
