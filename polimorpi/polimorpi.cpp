040050e77a400c87e1a22a161e3e31b9256c6da5
wpiskaa <hafizzkurniawan19@gmail.com>
2025-06-02 11:09:00 +0700

    membuat class seseorang beserta fungsi didalamnya

--- polimorpi/polimorpi.cpp
+++ polimorpi/polimorpi.cpp
@@ -1,2 +1,10 @@
 #include <iostream>
 using namespace std;
+
+class seseorang {
+    public:
+    virtual void pesan() = 0;
+    //virtual void pesan(){
+    //  cout << "Pesan dari seseorang" << endl;
+    // }
+};
